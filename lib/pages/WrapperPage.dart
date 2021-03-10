8699f27174b9e815f793f9077c31fe5c15bddbb8
Lwazi Ndlovu <42185877+lwazin@users.noreply.github.com>
2021-03-10 15:53:47 +0200

    Updating personal site

--- lib/pages/WrapperPage.dart
+++ lib/pages/WrapperPage.dart
@@ -18,85 +18,107 @@ class _WrapperPageState extends State<WrapperPage> {
       body: Container(
         child: Stack(
           children: [
-            Center(
-              child: AnimatedOpacity(
-                opacity: (logoOn) ? 1.0 : 0.2,
-                duration: Duration(
-                  milliseconds: (logoOn) ? 80 : 10,
-                ),
-                child: ColorFiltered(
-                  colorFilter: ColorFilter.mode(
-                    (logoOn) ? Colors.deepOrange : Colors.white,
-                    BlendMode.modulate,
-                  ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      GestureDetector(
-                        onTap: () {
-                          setState(() {
-                            logoOn = !logoOn;
-                          });
-                        },
-                        child: AnimatedContainer(
-                          duration: Duration(
-                            milliseconds: (logoOn) ? 10 : 100,
-                          ),
-                          child: Image.asset(
-                            "images/logo1.png",
-                            // height: 200,
-                            fit: BoxFit.cover,
-                          ),
-                          width: (logoOn) ? 160 : 150,
-                        ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Center(
+                  child: AnimatedOpacity(
+                    opacity: (logoOn) ? 1.0 : 0.2,
+                    duration: Duration(
+                      milliseconds: (logoOn) ? 80 : 10,
+                    ),
+                    child: ColorFiltered(
+                      colorFilter: ColorFilter.mode(
+                        (logoOn) ? Colors.deepOrange : Colors.white,
+                        BlendMode.modulate,
                       ),
-                      AnimatedOpacity(
-                        duration: Duration(
-                          milliseconds: (logoOn) ? 400 : 10,
-                        ),
-                        opacity: (logoOn) ? 1.0 : 0.0,
-                        child: AnimatedContainer(
-                          width: 200,
-                          duration: Duration(
-                            milliseconds: 200,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          GestureDetector(
+                            onTap: () {
+                              setState(() {
+                                logoOn = !logoOn;
+                              });
+                            },
+                            child: AnimatedContainer(
+                              duration: Duration(
+                                milliseconds: (logoOn) ? 10 : 100,
+                              ),
+                              child: Image.asset(
+                                "images/logo1.png",
+                                // height: 200,
+                                fit: BoxFit.cover,
+                              ),
+                              width: (logoOn) ? 160 : 150,
+                            ),
                           ),
-                          child: Container(
-                            child: Center(
-                              child: Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  Text(
-                                    "lwazi",
-                                    style: GoogleFonts.oswald(
-                                      color: Colors.black,
-                                      fontWeight: FontWeight.w300,
-                                      fontSize: 30,
-                                    ),
+                          AnimatedOpacity(
+                            duration: Duration(
+                              milliseconds: (logoOn) ? 400 : 10,
+                            ),
+                            opacity: (logoOn) ? 1.0 : 0.0,
+                            child: AnimatedContainer(
+                              width: 200,
+                              duration: Duration(
+                                milliseconds: 200,
+                              ),
+                              child: Container(
+                                child: Center(
+                                  child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      Text(
+                                        "lwazi",
+                                        style: GoogleFonts.oswald(
+                                          color: Colors.black,
+                                          fontWeight: FontWeight.w300,
+                                          fontSize: 30,
+                                        ),
+                                      ),
+                                      AnimatedContainer(
+                                        duration: Duration(
+                                          milliseconds: (logoOn) ? 200 : 400,
+                                        ),
+                                        width: (logoOn) ? 0 : 8,
+                                      ),
+                                      Text(
+                                        "N",
+                                        style: GoogleFonts.oswald(
+                                          color: Colors.deepOrange,
+                                          fontWeight: FontWeight.w700,
+                                          fontSize: 30,
+                                        ),
+                                      ),
+                                    ],
                                   ),
-                                  AnimatedContainer(
-                                    duration: Duration(
-                                      milliseconds: (logoOn) ? 200 : 400,
-                                    ),
-                                    width: (logoOn) ? 0 : 8,
-                                  ),
-                                  Text(
-                                    "N",
-                                    style: GoogleFonts.oswald(
-                                      color: Colors.deepOrange,
-                                      fontWeight: FontWeight.w700,
-                                      fontSize: 30,
-                                    ),
-                                  ),
-                                ],
+                                ),
                               ),
                             ),
                           ),
-                        ),
+                        ],
                       ),
-                    ],
+                    ),
                   ),
                 ),
-              ),
+                Center(
+                  child: AnimatedContainer(
+                    width: 300,
+                    duration: Duration(milliseconds: 200),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.blueAccent,
+                          ),
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                        hintText: "Enabled decoration text ...",
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
           ],
         ),
